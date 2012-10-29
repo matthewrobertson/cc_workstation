c87cd5d62e0f3404fbd176f319953c695eea701d
Matthew Robertson <matthewrobertson03@gmail.com>
2012-10-29 11:44:01 -0700

    listed pivotal_workstation dependency

--- metadata.rb
+++ metadata.rb
@@ -1,5 +1,6 @@
 maintainer        "Cloud Clinic"
 maintainer_email  "matthew@cloudclinic.ca"
+depends           "pivotal_workstation"
 description       "Configure an OSX workstation of Rails development"
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.markdown'))
 version           "1.0.0"
